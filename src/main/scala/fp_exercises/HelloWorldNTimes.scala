10c94d8ed42090b24d554ce10321d2f93eb6ad28
tim.shen <tim.shen@farfetch.com>
2022-07-29 09:41:26 +0800

    add fp

---
+++ src/main/scala/fp_exercises/HelloWorldNTimes.scala
@@ -0,0 +1,27 @@
+package fp_exercises
+
+object HelloWorldNTimes extends App {
+  def f(i: Int) =
+    (1 to i).foreach(_ => println("Hello World"))
+
+  var n = scala.io.StdIn.readInt
+  f(n)
+}
+
+def f1(num:Int,arr:List[Int]):List[Int] =
+  arr.flatMap(i => List.fill[Int](num)(i))
+
+def f2(delim:Int,arr:List[Int]):List[Int] =
+  arr.filter(_ < delim)
+
+def f3(arr:List[Int]):List[Int] =
+  arr.zipWithIndex.collect{case (e, id) if id % 2 == 1 => e} // Can only do this way
+
+def f4(num:Int) : List[Int] = {
+  import scala.io.StdIn._
+  val list = (0 until num).toList
+  //println(s"[${list.mkString(", ")}]")
+  list
+}
+
+
---
+++ src/main/scala/fp_exercises/test1.sc
@@ -0,0 +1,2 @@
+io.Source.stdin.getLines().take(2).map(_.toInt).sum
+
